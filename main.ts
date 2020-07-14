59e98fdf745776380543daa22d21ac28bc97ed04
Itoga, Hibiki <itogahibiki@gmail.com>
2020-07-14 14:57:32 +0900

    added first loop

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
-let スプライト: game.LedSprite[] = []
-スプライト.push(game.createSprite(0, 0))
-スプライト.push(game.createSprite(1, 0))
-スプライト.push(game.createSprite(2, 0))
-スプライト.push(game.createSprite(3, 0))
-スプライト.push(game.createSprite(4, 0))
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        for (let index = 0; index <= 4; index++) {
+            S[idx].set(LedSpriteProperty.Y, index)
+            basic.pause(200)
+        }
+    }
+})
 let idx = 0
+let S: game.LedSprite[] = []
+S.push(game.createSprite(0, 0))
+S.push(game.createSprite(1, 0))
+S.push(game.createSprite(2, 0))
+S.push(game.createSprite(3, 0))
+S.push(game.createSprite(4, 0))
+idx = 0
 basic.forever(function () {
 	
 })
