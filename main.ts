702be3a15d7dec796dbd6e8e119ed32bc0344ceb
Itoga, Hibiki <itogahibiki@gmail.com>
2020-07-14 14:51:33 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let スプライト: game.LedSprite[] = []
+スプライト.push(game.createSprite(0, 0))
+スプライト.push(game.createSprite(1, 0))
+スプライト.push(game.createSprite(2, 0))
+スプライト.push(game.createSprite(3, 0))
+スプライト.push(game.createSprite(4, 0))
+let idx = 0
+basic.forever(function () {
+	
+})
