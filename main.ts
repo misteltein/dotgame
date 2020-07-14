a6fdc8914f9925f9a86da396c295ca8f7c87c216
Itoga, Hibiki <itogahibiki@gmail.com>
2020-07-14 15:43:56 +0900

    WIP

--- main.ts
+++ main.ts
@@ -1,23 +1,33 @@
 input.onButtonPressed(Button.A, function () {
-    while (idx_sprite <= 4) {
+    while (true) {
         for (let index = 0; index <= 4; index++) {
+            if (idx_sprite >= 5) {
+                break;
+            }
             S[idx_sprite].set(LedSpriteProperty.Y, index)
             basic.pause(200)
         }
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
+})
 input.onButtonPressed(Button.B, function () {
     if (idx_sprite == 4) {
+        idx_sprite = 5
         for (let i = 0; i <= 4; i++) {
             if (S[i].get(LedSpriteProperty.Y) != 2) {
                 basic.showIcon(IconNames.No)
-                control.reset()
+                basic.showIcon(IconNames.No)
+                basic.showIcon(IconNames.No)
             }
-            basic.showIcon(IconNames.Diamond)
-            control.reset()
         }
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.Diamond)
     }
     idx_sprite += 1
+    music.playTone(262, music.beat(BeatFraction.Whole))
 })
 let idx_sprite = 0
 let S: game.LedSprite[] = []
