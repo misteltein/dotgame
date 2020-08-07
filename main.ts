97085227e5318496c1bba934694a32dbe2c961f5
Itoga, Hibiki <itogahibiki@gmail.com>
2020-08-07 16:13:12 +0900

    debuged

--- main.ts
+++ main.ts
@@ -9,17 +9,16 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    control.reset()
-})
 input.onButtonPressed(Button.B, function () {
     if (idx_sprite == 4) {
         idx_sprite = 5
         for (let i = 0; i <= 4; i++) {
             if (S[i].get(LedSpriteProperty.Y) != 2) {
+                success = 0
                 basic.showIcon(IconNames.No)
             }
         }
+        success = 1
         basic.showIcon(IconNames.Diamond)
     }
     idx_sprite += 1
@@ -27,9 +26,18 @@ input.onButtonPressed(Button.B, function () {
 })
 let idx_sprite = 0
 let S: game.LedSprite[] = []
+let success = 0
+success = 2
 S.push(game.createSprite(0, 0))
 S.push(game.createSprite(1, 0))
 S.push(game.createSprite(2, 0))
 S.push(game.createSprite(3, 0))
 S.push(game.createSprite(4, 0))
 idx_sprite = 0
+basic.forever(function () {
+    if (success == 1) {
+        music.playTone(523, music.beat(BeatFraction.Whole))
+    } else if (success == 0) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+})
